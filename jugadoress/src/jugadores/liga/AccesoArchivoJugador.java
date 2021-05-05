38f1e95da7150fff550711b72e56209d9f7d7b83
ivanm <ivanm@DESKTOP-58SEU4V>
2021-05-05 18:45:10 +0200

    Guardar jugadores

---
+++ jugadoress/src/jugadores/liga/AccesoArchivoJugador.java
@@ -0,0 +1,61 @@
+package jugadores.liga;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class AccesoArchivoJugador {
+/**
+ * Escribe en el archivo indicado.
+ * @param rutaArchivo Ruta donde se encuentra el archivo
+ * @param texto Texto que se escribe en el archivo
+ */
+	public static void escribir(String rutaArchivo, ArrayList<String> texto) {
+		File refArchivo = new File(rutaArchivo);
+		try {
+			FileWriter fs = new FileWriter(refArchivo);
+			PrintWriter out = new PrintWriter(fs);
+			for (int i = 0; i < texto.size(); i++) {
+				out.println(texto.get(i));
+			}
+				out.close();
+		} catch (FileNotFoundException e) {
+			System.err.printf("Ruta no encontrada: %s", e.getLocalizedMessage());
+		} catch (IOException e) {
+			System.err.printf("Error de escritura: %s", e.getLocalizedMessage());
+		}
+
+	}
+/**
+ * Lee el archivo txt y lo guarda en una matriz de cadenas de texto.
+ * @return copiarTexto
+ */
+	public static ArrayList<String> leer() {
+		
+		ArrayList<String> copiarTexto = new ArrayList<String>();
+		File refArchivo = new File("jugadores.txt");
+		String textoFila="";
+		try {
+			FileReader fr = new FileReader(refArchivo);
+			BufferedReader in = new BufferedReader(fr);
+			while(textoFila != null) {
+				textoFila=in.readLine();
+				if(textoFila != null) {
+					copiarTexto.add(textoFila);
+				}
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+			System.err.printf("Ruta no encontrada: %s", e.getLocalizedMessage());
+		} catch (IOException e) {
+			System.err.printf("Error de lectura: %s", e.getLocalizedMessage());
+		}
+		return copiarTexto;
+	}
+	
+}
